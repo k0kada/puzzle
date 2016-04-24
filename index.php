d4d391f83cdd413ca69d86b3d2794b5fe2f7c461
Koh Okada <koh.okada@gmail.com>
2016-04-24 09:01:51 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div id="title">puzzle</div>
+    <canvas id="game_canvas" width="320" height="320"></canvas>
+    <script type="text/javascript">
+    </script>
+  </body>
+</html>
