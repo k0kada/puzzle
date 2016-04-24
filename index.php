d93708163fa5fc0e1c32aec5b9b057a359da7a22
Koh Okada <koh.okada@gmail.com>
2016-04-24 09:51:22 +0000

    jsで画像表示

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 ?>
 
 <!DOCTYPE html>
@@ -9,8 +8,22 @@
   </head>
   <body>
     <div id="title">puzzle</div>
-    <canvas id="game_canvas" width="320" height="320"></canvas>
+    <canvas id="canvas" width="320" height="320"></canvas>
     <script type="text/javascript">
+      //パズル画像
+      var main_image = "mario.png";
+      //Canvas
+      var canvas = document.getElementById("canvas");
+
+      //画像の読み込み
+      var image = new Image();
+      image.src = main_image;
+
+      image.onload = function() {
+        //2dコンテクスト
+        var con = canvas.getContext("2d");
+        con.drawImage(image, 0, 0, 320, 320, 0, 0, 320, 320);
+      }
     </script>
   </body>
 </html>
